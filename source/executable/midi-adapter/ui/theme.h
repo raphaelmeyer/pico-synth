cce9755b1dc9410ea7e307604c4f4b36890d86b2
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-01-14 15:47:00 +0100

    Tweak ui colors

--- source/executable/midi-adapter/ui/theme.h
+++ source/executable/midi-adapter/ui/theme.h
@@ -5,8 +5,8 @@
 namespace theme {
 
 const lv_color_t bg_color = lv_color_hex(0x000000);
-const lv_color_t text_color = lv_color_hex(0xFFFFFF);
-const lv_color_t selected_color = lv_color_hex(0xFF8000);
-const lv_color_t edit_color = lv_color_hex(0x80FFFF);
+const lv_color_t text_color = lv_color_hex(0xD0D0FF);
+const lv_color_t selected_color = lv_color_hex(0x00FF80);
+const lv_color_t edit_color = lv_color_hex(0xFFD000);
 
 } // namespace theme
