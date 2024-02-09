ce746ac3c74668d0b66efb2d29f9b2b162c6b6e7
raphael meyer <raphael.akira.meyer@gmail.com>
2024-02-09 22:38:42 +0100

    Migrate envelope generator

--- source/shared/synth/envelope-generator.cc
+++ source/shared/synth/envelope-generator.cc
@@ -1,3 +1,84 @@
 #include "envelope-generator.h"
 
-EnvelopeGenerator::EnvelopeGenerator(Source &source) : source_{source} {}
+EnvelopeGenerator::EnvelopeGenerator(uint32_t sampling_rate, Source &source)
+    : sampling_rate_{sampling_rate}, source_{source} {}
+
+uint16_t EnvelopeGenerator::next_value() {
+
+  if (state_ == State::Idle) {
+    return 0;
+  }
+
+  uint32_t level = 0;
+
+  if (state_ == State::Attack) {
+    if (ticks_ > attack_) {
+      ticks_ = 0;
+      state_ = State::Decay;
+    } else {
+      level = (65535 * ticks_) / attack_;
+    }
+  }
+
+  if (state_ == State::Decay) {
+    if (ticks_ > decay_) {
+      ticks_ = 0;
+      state_ = State::Sustain;
+    } else {
+      level = 65535 - ((65535 - sustain_) * ticks_) / decay_;
+    }
+  }
+
+  if (state_ == State::Sustain) {
+    level = sustain_;
+  }
+
+  if (state_ == State::Release) {
+    if (ticks_ > release_) {
+      ticks_ = 0;
+      state_ = State::Idle;
+    } else {
+      level = sustain_ - (sustain_ * ticks_) / release_;
+    }
+  }
+
+  ++ticks_;
+
+  auto const value = (source_.next_value() * level) / 65535;
+  return (value * volume_) / 65535;
+}
+
+void EnvelopeGenerator::trigger() {
+  state_ = State::Attack;
+  ticks_ = 0;
+}
+
+void EnvelopeGenerator::release() {
+  switch (state_) {
+  case State::Attack:
+  case State::Decay:
+  case State::Sustain:
+    state_ = State::Release;
+    ticks_ = 0;
+
+  case State::Idle:
+  case State::Release:
+    break;
+  }
+}
+
+void EnvelopeGenerator::set_attack(uint16_t ms) {
+  attack_ = ms * sampling_rate_ / 1000;
+}
+
+void EnvelopeGenerator::set_decay(uint16_t ms) {
+  decay_ = ms * sampling_rate_ / 1000;
+}
+
+void EnvelopeGenerator::set_sustain(uint16_t level) { sustain_ = level; }
+
+void EnvelopeGenerator::set_release(uint16_t ms) {
+  release_ = ms * sampling_rate_ / 1000;
+}
+
+void EnvelopeGenerator::set_volume(uint16_t volume) { volume_ = volume; }
--- source/shared/synth/include/synth/synth/envelope-generator.h
+++ source/shared/synth/include/synth/synth/envelope-generator.h
@@ -1,11 +1,34 @@
 #pragma once
 
-class Source;
+#include "source.h"
 
-class EnvelopeGenerator {
+class EnvelopeGenerator : public Source {
 public:
-  EnvelopeGenerator(Source &source);
+  EnvelopeGenerator(uint32_t sampling_rate, Source &source);
+
+  uint16_t next_value() override;
+
+  void trigger();
+  void release();
+
+  void set_attack(uint16_t attack);
+  void set_decay(uint16_t decay);
+  void set_sustain(uint16_t sustain);
+  void set_release(uint16_t release);
+  void set_volume(uint16_t volume);
 
 private:
+  uint32_t const sampling_rate_;
   Source &source_;
+
+  enum class State { Idle, Attack, Decay, Sustain, Release };
+  State state_{State::Idle};
+
+  int32_t attack_{};
+  int32_t decay_{};
+  uint16_t sustain_{};
+  int32_t release_{};
+  int32_t volume_{};
+
+  int32_t ticks_{};
 };
