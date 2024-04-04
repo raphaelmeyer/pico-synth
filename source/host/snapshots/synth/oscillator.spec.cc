a4455ecb4100e109a151dfd782bb33ba6cf59b12
raphael meyer <raphael.akira.meyer@gmail.com>
2024-04-04 21:31:33 +0200

    Snapshot tests for oscillators

---
+++ source/host/snapshots/synth/oscillator.spec.cc
@@ -0,0 +1,83 @@
+#include <ApprovalTests.hpp>
+#include <doctest/doctest.h>
+
+#include <synth/device/random.h>
+#include <synth/synth/oscillator.h>
+
+#include <algorithm>
+
+namespace {
+
+class FakeRandom : public Random {
+public:
+  uint32_t value() override {
+    ++value_;
+    value_ %= 16;
+    return value_ * 15000;
+  }
+
+private:
+  uint32_t value_{};
+};
+
+TEST_CASE("square") {
+  FakeRandom random{};
+  Oscillator oscillator{48'000, random};
+
+  oscillator.set_type(WaveForm::Square);
+  oscillator.set_frequency(22222);
+
+  std::vector<uint16_t> values{};
+  values.resize(500);
+  std::ranges::generate(values,
+                        [&oscillator] { return oscillator.next_value(); });
+
+  ApprovalTests::Approvals::verifyAll(values);
+}
+
+TEST_CASE("triangle") {
+  FakeRandom random{};
+  Oscillator oscillator{48'000, random};
+
+  oscillator.set_type(WaveForm::Triangle);
+  oscillator.set_frequency(1234);
+
+  std::vector<uint16_t> values{};
+  values.resize(500);
+  std::ranges::generate(values,
+                        [&oscillator] { return oscillator.next_value(); });
+
+  ApprovalTests::Approvals::verifyAll(values);
+}
+
+TEST_CASE("sawtooth") {
+  FakeRandom random{};
+  Oscillator oscillator{48'000, random};
+
+  oscillator.set_type(WaveForm::Sawtooth);
+  oscillator.set_frequency(8765);
+
+  std::vector<uint16_t> values{};
+  values.resize(500);
+  std::ranges::generate(values,
+                        [&oscillator] { return oscillator.next_value(); });
+
+  ApprovalTests::Approvals::verifyAll(values);
+}
+
+TEST_CASE("noise") {
+  FakeRandom random{};
+  Oscillator oscillator{48'000, random};
+
+  oscillator.set_type(WaveForm::Noise);
+  oscillator.set_frequency(1760);
+
+  std::vector<uint16_t> values{};
+  values.resize(500);
+  std::ranges::generate(values,
+                        [&oscillator] { return oscillator.next_value(); });
+
+  ApprovalTests::Approvals::verifyAll(values);
+}
+
+} // namespace
